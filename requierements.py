5b6cb41d2e697c8cf1f2dbd94844d0bdcec2f1cc
bigponche <andresdbgt3@gmail.com>
2026-08-11 18:39:12 -0400

    feat: define architecture and requirements for BMI calculator

---
+++ requierements.py
@@ -0,0 +1,10 @@
+UNDERWEIGHT_LIMIT = 18.5    
+NORMAL_LIMIT = 25.0    
+OVERWEIGHT_LIMIT = 30.0     
+                            
+
+CONVERSION = 703                  
+
+#El usuario debe definir en que sistema trabaja metric or imperial
+#He must enter valid data(negative numbers, divisionzero, etc are discarded)
+#he must give at least 2 values, weigth and heigth
